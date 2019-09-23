9ae2889f865ae45a675676134f09d5f6115342e4
Afsar <afsaredrisz@icloud.com>
2019-09-23 12:17:32 +0530

    initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-TicTokLikeTableView_Example/Pods-TicTokLikeTableView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TicTokLikeTableView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TicTokLikeTableView_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-TicTokLikeTableView_Tests/Pods-TicTokLikeTableView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TicTokLikeTableView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TicTokLikeTableView_Tests
+@end
